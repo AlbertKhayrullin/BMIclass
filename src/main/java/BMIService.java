bb665887cd37be28db8667c5d03cc0cbf5be5f0f
AlbertKhayrullin <albertKh1620@rambler.ru>
2021-02-25 13:59:27 +0300

    fixed #1

--- src/main/java/BMIService.java
+++ src/main/java/BMIService.java
@@ -1,6 +1,6 @@
 public class BMIService {
     double calculate(double height, double weight) {
-        double index = weight / (height * height / 10000);
+        double index = weight / ((height / 100) * (height / 100));
         double scale = Math.pow(10, 2);
         double result = Math.ceil(index * scale) / scale;
         return result;
